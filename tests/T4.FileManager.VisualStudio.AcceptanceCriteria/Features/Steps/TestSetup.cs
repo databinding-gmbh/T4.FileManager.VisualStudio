18e14eba8c6bfe703c4008dafac20c94d5cca4e1
R L <renegadexx@gmx.net>
2021-01-10 12:54:45 +0100

    ScreenCapture added

--- tests/T4.FileManager.VisualStudio.AcceptanceCriteria/Features/Steps/TestSetup.cs
+++ tests/T4.FileManager.VisualStudio.AcceptanceCriteria/Features/Steps/TestSetup.cs
@@ -1,5 +1,8 @@
 ﻿namespace T4.FileManager.VisualStudio.AcceptanceCriteria.Features.Steps
 {
+    using System.Drawing.Imaging;
+    using System.IO;
+
     using T4.FileManager.VisualStudio.AcceptanceCriteria.Features.Helper;
 
     using TechTalk.SpecFlow;
@@ -11,6 +14,14 @@
         public static void AfterTestRun()
         {
             VisualStudioHelper.CleanupViaT4();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            var path = "_" + Path.GetFileNameWithoutExtension(Path.GetTempFileName()) + ".png";
+            var img = new ScreenCapture().CaptureScreen();
+            img.Save(path, ImageFormat.Png);
         }
     }
 }
